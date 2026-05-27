818aed43e31b46f994f94fb77ab9e4dcf5b0a1de
OscarCrs <zcrscortez@gmail.com>
2026-05-26 20:57:57 -0600

    Actualizacion del simulador de cafeteria

---
+++ Simulador_cafeteria/BebidaCaliente.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulador_cafeteria
+{
+    internal class BebidaCaliente : Bebida
+    {
+        private float Temperatura;
+
+        public float temperatura
+        {
+            get {  return Temperatura; } 
+            set { Temperatura = value; }
+        }
+
+        //constructor que hereda
+        public BebidaCaliente(string _nombre, string _tamaño, float _temperatura, float _precio):base(_nombre,_tamaño,_precio)
+        {
+            Temperatura = _temperatura;
+        }
+
+        //metodo sobreescrito(Polimorfismo)
+        public override string Preparar()
+        {
+            return "\n Preparando un  " + Nombre + " Caliente a una temperatura de: "+Temperatura + ", tamaño : " + Tamaño ;
+        } 
+
+    }
+}
---
+++ Simulador_cafeteria/BebidaFria.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Simulador_cafeteria
+{
+    internal class BebidaFria : Bebida
+    {
+        private int CantidadHielo;
+
+        public int cantidadHielo
+        {
+            get { return CantidadHielo; }
+            set
+            {
+                if (value > 0)
+                {
+                    CantidadHielo = value;
+                }
+                else
+                {
+                    CantidadHielo = -1;
+                }
+                
+            }
+        }
+
+        public BebidaFria(string _nombre, string _tamaño, int _cantidadHielo, float _precio) : base(_nombre, _tamaño, _precio)
+        {
+            CantidadHielo = _cantidadHielo;
+        }
+
+        //metodo sobreescrito(Polimorfismo)
+        public override string Preparar()
+        {
+            return "\n Preparando un : " + Nombre + " helado con " +CantidadHielo + " cubos de hielo, tamaño : " + Tamaño;
+        }
+    }
+}
